7536809e99cc2c4ba3c5abc58758dbc811d82363
Kailas Rathod <53994455+kailas-rathod@users.noreply.github.com>
2023-01-13 13:27:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React, { useState } from "react";
 import "./styles.css";
 
+const Err_MSG = " Age must be greater than 18";
+
 export default function App() {
+  const [text, setText] = useState("");
+  const [isError, setError] = useState(false);
+  const handleTextinput = (e) => {
+    setError(false)
+    console.log(e.target.value);
+    setText(e.target.value);
+    if (e.target.value < 18) {
+      setError(true);
+    }
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <input value={text} onChange={handleTextinput} />
+      {isError ? Err_MSG : null}
     </div>
   );
 }
